a2666b969fc570b915dfe84668d8ae1a4355d359
AntonioE3 <112135000+CoolKidAnt@users.noreply.github.com>
2022-09-13 21:41:28 +0000

    addaed more to the dictionary and tryint to setup best answer thing

--- dic.py
+++ dic.py
@@ -1,9 +1,13 @@
 scene = {
- 1: "\n You are driving home with your family and you are in a rural area in the mountains. As you are driving you see a big white ball of light crashing and falling into the woods. What do you do? \n\n (a) Drive away as quickly as possible \n (b) Go and check out whatever fell into the woods \n (c) Unalive yourself because you know your gnna die" ,
+ 1:{ "\n You are driving home with your family and you are in a rural area in the mountains. As you are driving you see a big white ball of light crashing and falling into the woods. What do you do? \n\n (a) Drive away as quickly as possible \n (b) Go and check out whatever fell into the woods \n (c) Unalive yourself because you know your gnna die",
+"best"
+    
+},
 
 2:"You are now at home and you sit down and turn on the TV. Breaking news pops up and says that an unidnetified object has crashed in the woods. You know that the object came from outer space. What are your next steps? \n\n (a) start making calls and check on your family \n (b) Go to your local mall and gather a bunch of supplies then run \n (c) Join the military in order to fight the aliens", 
 
- 3:"Now at the mountains you are in a rural area. The mountain is filled with dangerous animals like snakes",
+ 3:"Panic has ensued the city news of the alien invasion has been leaked and extraterestrials have been filmed going out of the Unidintefied object. The King of England tried to make contact but to no avail. The aliens open fire and kills everyone in a 10 km radius. Your closest and dearest friend somehow contacted you and tells you that he/she is trapped. What do you do? /n/n (a) Run back to the city ASAP and try to rescue friend /n (b) ignore him completly and just go on your merry way. (c) Answer him and tell him your not coming ",
+  
 
  4:"",
 
@@ -15,27 +19,27 @@ scene = {
 }
 
 answerp1={
-  "a":"asdfads",
+  "a":"Driving away was the best option as you'll learn later",
   
-  "b":"adsf",
+  "b":"Aprroaching the object you try and touch it. As your fingers were centimetres away from touching it you feel this electric jolt going through your body. Before you know it you've exploded into a billion pieces",
 
-  "c":"aspfiu"
+  "c":"Well that was a dumb move, you clearly give up to easily."
 }
 
 answerp2={
-  "a":"asdfads",
+  "a":"Calling your family did nothing but add more things to worry about in your head. As your packing you forget a bunch of important things. As your thinking about other people too much",
   
-  "b":"adsf",
+  "b":"This was a smart move. You went to the store and only grabbed the things you needed. And ran to the mountains to hide. Later learning that everyone in that town was killed.",
 
-  "c":"aspfiu"
+  "c":"1 week into the military you get absolutely eradicated by a loose missle",
 }
 
 answerp3={
-  "a":"asdfads",
+  "a":"Blindly running into the city you dont realize that the aliens knew you were already coming killing you way before you got close into town in the first place",
   
-  "b":"adsf",
+  "b":"This was the best option althoUGH the thought of never seeing your best friend will haunt you for the rest of your life knowing that you could've prevented it. ",
 
-  "c":"aspfiu"
+  "c":"When you ignore your friend it activates this rage inside him that makes him want nothing more than to kill you. 1 month later as your sleeping in your tent in the mountain. Your friend crawls into your tent and slits your throat"
 }
 
 answerp4={
